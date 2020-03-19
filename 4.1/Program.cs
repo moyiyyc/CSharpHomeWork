9c64a6373f079cf8e1fe2f564be511311a8b442e
moyi <moyi@DESKTOP-RMRP4B4>
2020-03-19 18:35:56 +0800

    第四次作业

---
+++ 4.1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> x = head; x != null; x = x.Next)
+            {
+                action(x.Data);
+            }
+
+        }
+
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            int min = 0x7fffffff, max = 0x0, sum = 0;
+
+            intlist.ForEach(m => Console.WriteLine(m));
+            intlist.ForEach(m => { if (m < min) min = m; });
+            intlist.ForEach(m => { if (m > max) max = m; });
+            intlist.ForEach(m => sum += m);
+            Console.WriteLine($"{ min}, {max}, {sum}");
+
+
+
+
+
+        }
+
+    }
+}
---
+++ 4.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _4._2
+{
+    public delegate void action ();
+    public class Time
+    {
+        public event action Work;
+        public void Start()
+        {
+            Work();
+        }
+    }
+    public class AlarmClock
+    {
+        static int i=0;
+        public void TickAlarm(Time time)
+        {
+            
+            time.Work += Tick;
+            
+            
+            void Tick()
+            {
+                while (true)
+                {
+                    Console.WriteLine("滴答");
+                    System.Threading.Thread.Sleep(1000);
+                    i++;
+                    if (i % 10 == 0)
+                    {
+                        Console.WriteLine("响铃!!!");
+                    }
+                }
+                
+
+            }
+            
+        }
+    }   
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time = new Time();
+            AlarmClock alarmclock = new AlarmClock();
+            alarmclock.TickAlarm(time);
+            time.Start();
+
+        }
+    }
+}
