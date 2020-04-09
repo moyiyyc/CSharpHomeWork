cfdabc0f1faefb1ce64f1aec821bb138bdd7891e
moyi <moyi@DESKTOP-RMRP4B4>
2020-04-09 18:48:09 +0800

    1

---
+++ 5.1/Order.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _5._1
+{
+   public class Order
+    {
+       
+        private List<OrderItem> items;
+        public uint OrderId{ get; set; }
+        public string Customer { get; set; }
+        public DateTime CreateTime { get; set; }
+        public Order()
+        {
+            items = new List<OrderItem>();
+            CreateTime = DateTime.Now;
+        }
+        public Order(uint orderId, string customer, List<OrderItem> items)
+        {
+            this.OrderId = orderId;
+            this.Customer = customer;
+            this.items = (items == null) ? new List<OrderItem>() : items;
+        }
+
+        public List<OrderItem> Items
+        {
+            get { return items; }
+        }
+
+        public double TotalPrice
+        {
+            get => items.Sum(item => item.TotalPrice);
+        }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            if (Items.Contains(orderItem))
+                throw new ApplicationException($"orderItem-{orderItem} is already existed!");
+            Items.Add(orderItem);
+        }
+
+        public void RemoveItem(OrderItem orderItem)
+        {
+            Items.Remove(orderItem);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append($"Id:{OrderId}, customer:{Customer},orderTime:{CreateTime},totalPrice：{TotalPrice}");
+            items.ForEach(od => strBuilder.Append("\n\t" + od));
+            return strBuilder.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var order = obj as Order;
+            return order != null &&
+                   OrderId == order.OrderId;
+        }
+
+        public override int GetHashCode()
+        {
+            var
+            hashCode = +OrderId.GetHashCode()
+             + EqualityComparer<string>.Default.GetHashCode(Customer)
+            + CreateTime.GetHashCode();
+            return hashCode;
+        }
+
+        public int CompareTo(Order other)
+        {
+            if (other == null) return 1;
+            return this.OrderId.CompareTo(other.OrderId);
+        }
+
+    }
+    
+
+
+
+
+    
+   
+   
+}
---
+++ 5.1/OrderItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _5._1
+{
+    public class OrderItem
+    {
+        public uint Index { get; set; } //序号
+
+        public String GoodsName { get; set; }
+
+        public uint Quantity { get; set; }
+
+        public double Price { get; set; }
+
+        
+
+        public OrderItem(uint index, String goodsName, double price, uint quantity)
+        {
+            this.Index = index;
+            this.GoodsName = goodsName;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public double TotalPrice
+        {
+            get => Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"[编号:{Index},商品:{GoodsName},数目:{Quantity},总价:{TotalPrice}]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as OrderItem;
+            return item != null &&
+                   GoodsName == item.GoodsName;
+        }
+
+        public override int GetHashCode()
+        {
+            Console.WriteLine("shabi");
+            var hashCode =
+             Index.GetHashCode()
+             + EqualityComparer<string>.Default.GetHashCode(GoodsName)+
+             Quantity.GetHashCode();
+            return hashCode;
+        }
+    }
+}
